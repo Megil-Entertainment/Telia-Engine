5366b956b15efc7cc8dad88df07aa684e1f12903
samichr1 <samichr1@students.zhaw.ch>
2018-09-27 17:41:47 +0200

    add basic fxml

---
+++ src/main/java/ch/megil/teliaengine/Main.java
@@ -0,0 +1,27 @@
+package ch.megil.teliaengine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private static final String APP_NAME = "Telia-Engine";
+	private static final String MAIN_VIEW = "EngineUI.fxml";
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Pane root = FXMLLoader.load(getClass().getResource(MAIN_VIEW));
+		
+		var scene = new Scene(root, 1000, 500);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(APP_NAME);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
