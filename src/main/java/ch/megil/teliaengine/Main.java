0abad49bf25ecf86f3c8bf4d85811ac87b543bcf
samichr1 <samichr1@students.zhaw.ch>
2018-09-28 18:06:50 +0200

    organise & format

--- src/main/java/ch/megil/teliaengine/Main.java
+++ src/main/java/ch/megil/teliaengine/Main.java
@@ -9,19 +9,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	private static final String APP_NAME = "Telia-Engine";
 	private static final String MAIN_VIEW = "base/EngineUI.fxml";
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Pane root = FXMLLoader.load(getClass().getResource(MAIN_VIEW));
-		
+
 		var scene = new Scene(root);
-		
+
 		primaryStage.setMaximized(true);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle(APP_NAME);
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/main/java/ch/megil/teliaengine/base/EngineUIController.java
+++ src/main/java/ch/megil/teliaengine/base/EngineUIController.java
@@ -5,11 +5,11 @@ import javafx.fxml.FXML;
 public class EngineUIController {
 	@FXML
 	private void gameRun() {
-		//TODO: implement run
+		// TODO: implement run
 	}
-	
+
 	@FXML
 	private void helpAbout() {
-		//TODO: implement about
+		// TODO: implement about
 	}
 }
