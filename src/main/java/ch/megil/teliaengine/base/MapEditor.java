a8d379baa4aa9420f67fc954283c89137f30d502
Armando Müller <muellarm@students.zhaw.ch>
2018-09-30 16:20:06 +0200

    Second version for moving events

--- src/main/java/ch/megil/teliaengine/base/MapEditor.java
+++ src/main/java/ch/megil/teliaengine/base/MapEditor.java
@@ -1,5 +1,7 @@
 package ch.megil.teliaengine.base;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
@@ -7,10 +9,12 @@ public class MapEditor extends Pane{
 	private double mouseX;
 	private double mouseY;
 	
+	@FXML
 	public void setOnMouseDragged(MouseEvent event) {
 		onMousePressed(event);
-		double deltaX = event.getSceneX() - mouseX;
-		double deltaY = event.getSceneY() - mouseY;
+		var source = (Node) event.getSource();
+		double deltaX = source.getLayoutX() - mouseX;
+		double deltaY = source.getLayoutY() - mouseY;
 		relocate(getLayoutX() + deltaX, getLayoutY() + deltaY);
 		mouseX = event.getSceneX() ;
         mouseY = event.getSceneY() ;
@@ -19,5 +23,21 @@ public class MapEditor extends Pane{
 	private void onMousePressed(MouseEvent event) {
 		mouseX = event.getSceneX();
 		mouseY = event.getSceneY();
+	}
+	
+	@FXML
+	public void dragPane(MouseEvent event) {
+		if(event.isSecondaryButtonDown()) {
+			setOnMouseDragged(event);
+			event.consume();
+		}
+	}
+	
+	@FXML
+	public void dragNode(MouseEvent event) {
+		if(event.isPrimaryButtonDown()) {
+			setOnMouseDragged(event);
+			event.consume();
+		}
 	}
 }
