76d2d3dfbee1cf226279eea0cf00eb95b3a9a3c9
Armando Müller <muellarm@students.zhaw.ch>
2018-10-01 18:30:32 +0200

    Add listener to Map-Editor

--- src/main/java/ch/megil/teliaengine/ui/component/MapEditor.java
+++ src/main/java/ch/megil/teliaengine/ui/component/MapEditor.java
@@ -1,12 +1,28 @@
-package ch.megil.teliaengine.base;
+package ch.megil.teliaengine.ui.component;
 
+import javafx.collections.ListChangeListener;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class MapEditor extends Pane{
 	private double dx;
 	private double dy;
+	
+	public MapEditor() {
+		getChildren().addListener((ListChangeListener<Node>) c -> {
+			while(c.next()) {
+				c.getAddedSubList().forEach(n -> {
+					n.setOnMousePressed(this::onDragStart);
+					n.setOnMouseDragged(this::onDragNode);
+					});
+			}});
+		
+		getChildren().add(new Rectangle(50,50,Color.BLUE));
+		getChildren().add(new Rectangle(50,50,Color.RED));
+	}
 	
 	public void onDragStart(MouseEvent event) {
 		var source = (Node) event.getSource();
