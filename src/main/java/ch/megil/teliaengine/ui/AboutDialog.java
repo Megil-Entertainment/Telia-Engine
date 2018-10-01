9fde91c4a1170ab79ad9acd92002fa4d4990316f
samichr1 <samichr1@students.zhaw.ch>
2018-10-01 12:23:25 +0200

    add about text

--- src/main/java/ch/megil/teliaengine/ui/AboutDialog.java
+++ src/main/java/ch/megil/teliaengine/ui/AboutDialog.java
@@ -1,5 +1,6 @@
 package ch.megil.teliaengine.ui;
 
+import ch.megil.teliaengine.TeliaConfiguration;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TextArea;
@@ -7,17 +8,24 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 
 public class AboutDialog extends Dialog<Void> {
+	private static final String ABOUT_TITLE = "About " + TeliaConfiguration.APP_NAME;
+	private static final String ABOUT_TEXT = TeliaConfiguration.APP_NAME +
+			"\n\nVersion:\t" + TeliaConfiguration.APP_VERSION +
+			"\n\nMade by:\t" + TeliaConfiguration.APP_COMPANY + 
+			"\n\t\t\t" + TeliaConfiguration.APP_AUTHORS;
+	
 	public AboutDialog() {
 		initModality(Modality.APPLICATION_MODAL);
 		
 		var root = new VBox();
 		
-		var aboutText = new TextArea();
+		var aboutText = new TextArea(ABOUT_TEXT);
 		aboutText.setEditable(false);
 		aboutText.setWrapText(true);
 		aboutText.setFocusTraversable(false);
 		root.getChildren().add(aboutText);
 		
+		setTitle(ABOUT_TITLE);
 		getDialogPane().setContent(root);
 		getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
 	}
