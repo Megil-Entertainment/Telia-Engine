5df9b479ed940e7b1b5177356bfdfb8afeb34678
samichr1 <samichr1@students.zhaw.ch>
2018-10-01 12:03:37 +0200

    add basic about dialog

---
+++ src/main/java/ch/megil/teliaengine/ui/AboutDialog.java
@@ -0,0 +1,24 @@
+package ch.megil.teliaengine.ui;
+
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+
+public class AboutDialog extends Dialog<Void> {
+	public AboutDialog() {
+		initModality(Modality.APPLICATION_MODAL);
+		
+		var root = new VBox();
+		
+		var aboutText = new TextArea("Test bla bla bla \n dsalfkjasdf \n asldfkjsadl...");
+		aboutText.setEditable(false);
+		aboutText.setWrapText(true);
+		aboutText.setFocusTraversable(false);
+		root.getChildren().add(aboutText);
+		
+		getDialogPane().setContent(root);
+		getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+	}
+}
