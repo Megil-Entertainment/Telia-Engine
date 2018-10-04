64d70846eae1d53025be7a6f6fe5bf306e59fee9
samichr1 <samichr1@students.zhaw.ch>
2018-10-04 16:21:33 +0200

    add save button to ui

--- src/main/java/ch/megil/teliaengine/ui/EngineUIController.java
+++ src/main/java/ch/megil/teliaengine/ui/EngineUIController.java
@@ -1,8 +1,17 @@
 package ch.megil.teliaengine.ui;
 
+import ch.megil.teliaengine.ui.component.MapEditor;
 import javafx.fxml.FXML;
 
 public class EngineUIController {
+	@FXML
+	private MapEditor mapEditor;
+
+	@FXML
+	private void fileSave() {
+		// TODO: implement save
+	}
+	
 	@FXML
 	private void gameRun() {
 		// TODO: implement run
