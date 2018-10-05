16cc3f0e88bb5ebaa3892f6e2ed2cd36c46693a4
samichr1 <samichr1@students.zhaw.ch>
2018-10-05 13:54:15 +0200

    add map & gameobject

---
+++ src/main/java/ch/megil/teliaengine/game/GameObject.java
@@ -0,0 +1,45 @@
+package ch.megil.teliaengine.game;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Node;
+
+public class GameObject {
+	private String name;
+	private DoubleProperty posX;
+	private DoubleProperty posY;
+	private Node depiction;
+	
+	public GameObject(String name, Node depiction) {
+		this.name = name;
+		this.depiction = depiction;
+		
+		this.posX = new SimpleDoubleProperty(0);
+		this.posY = new SimpleDoubleProperty(0);
+		
+		this.depiction.layoutXProperty().bindBidirectional(posX);
+		this.depiction.layoutYProperty().bindBidirectional(posY);
+	}
+	
+	public GameObject(String name, Node depiction, double posX, double posY) {
+		this(name, depiction);
+		this.posX.set(posX);
+		this.posY.set(posY);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPosX() {
+		return posX.get();
+	}
+
+	public double getPosY() {
+		return posY.get();
+	}
+
+	public Node getDepiction() {
+		return depiction;
+	}
+}
