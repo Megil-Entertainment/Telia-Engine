0950229bcf91602a32029675177ddb841d75f8a6
samichr1 <samichr1@students.zhaw.ch>
2018-10-08 17:34:21 +0200

    cleanup test

--- src/test/java/ch/megil/teliaengine/file/GameObjectSaveLoadTest.java
+++ src/test/java/ch/megil/teliaengine/file/GameObjectSaveLoadTest.java
@@ -1,13 +1,12 @@
 package ch.megil.teliaengine.file;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -23,9 +22,9 @@ public class GameObjectSaveLoadTest {
 
 	@Rule
 	public TemporaryFolder testMapsDir = new TemporaryFolder(parentDir);
-	
+
 	private GameObjectSaveLoad gameObjectSaveLoad;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		if (!parentDir.exists()) {
@@ -33,36 +32,28 @@ public class GameObjectSaveLoadTest {
 		}
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
 	@Before
 	public void setUp() throws Exception {
 		gameObjectSaveLoad = new GameObjectSaveLoad();
-		
+
 		var red = testMapsDir.newFile("red.tobj");
 		try (var writer = new BufferedWriter(new FileWriter(red))) {
 			writer.write("50.0/60.0/FF0000");
 		}
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testLoad() {
 		var optObj = gameObjectSaveLoad.load(testMapsDir.getRoot().getName() + "/red");
-		
+
 		assertTrue(optObj.isPresent());
 		var obj = optObj.get();
-		
+
 		assertEquals(testMapsDir.getRoot().getName() + "/red", obj.getName());
 		assertEquals(0.0, obj.getPosX(), 0);
 		assertEquals(0.0, obj.getPosY(), 0);
-		assertEquals(50.0, ((Rectangle)obj.getDepiction()).getWidth(), 0);
-		assertEquals(60.0, ((Rectangle)obj.getDepiction()).getHeight(), 0);
-		assertEquals(Color.RED, ((Rectangle)obj.getDepiction()).getFill());
+		assertEquals(50.0, ((Rectangle) obj.getDepiction()).getWidth(), 0);
+		assertEquals(60.0, ((Rectangle) obj.getDepiction()).getHeight(), 0);
+		assertEquals(Color.RED, ((Rectangle) obj.getDepiction()).getFill());
 	}
 }
