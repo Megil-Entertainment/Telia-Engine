c5f85595fb0893336716a893755b03d23e89a4e4
Armando Müller <muellarm@students.zhaw.ch>
2018-10-08 15:57:12 +0200

    Move rectangle from objectExplorer to MapEditor

--- src/main/java/ch/megil/teliaengine/ui/EngineUIController.java
+++ src/main/java/ch/megil/teliaengine/ui/EngineUIController.java
@@ -1,8 +1,20 @@
 package ch.megil.teliaengine.ui;
 
+import ch.megil.teliaengine.ui.component.MapEditor;
+import ch.megil.teliaengine.ui.component.ObjectEditor;
 import javafx.fxml.FXML;
 
 public class EngineUIController {
+	@FXML
+	private MapEditor mapEditor;
+	@FXML
+	private ObjectEditor objectExplorer;
+	
+	@FXML
+	private void initialize() {
+		objectExplorer.setMapEditor(mapEditor);
+	}
+	
 	@FXML
 	private void gameRun() {
 		// TODO: implement run
