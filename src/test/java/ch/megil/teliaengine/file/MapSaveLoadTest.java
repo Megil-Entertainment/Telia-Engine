490833381964f685078ef620e88d4470c11e0931
samichr1 <samichr1@students.zhaw.ch>
2018-10-10 17:29:08 +0200

    add map load & test

--- src/test/java/ch/megil/teliaengine/file/MapSaveLoadTest.java
+++ src/test/java/ch/megil/teliaengine/file/MapSaveLoadTest.java
@@ -5,8 +5,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -18,14 +20,19 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 
 import ch.megil.teliaengine.configuration.GameConfiguration;
+import ch.megil.teliaengine.file.exception.AssetFormatException;
+import ch.megil.teliaengine.file.exception.AssetNotFoundException;
 import ch.megil.teliaengine.game.GameObject;
 import ch.megil.teliaengine.game.Map;
 
 public class MapSaveLoadTest {
 	private static File parentDir = new File(GameConfiguration.ASSETS_MAPS.getConfiguration());
+	private static File objParentDir = new File(GameConfiguration.ASSETS_OBJECTS.getConfiguration());
 	
 	@Rule
 	public TemporaryFolder testMapsDir = new TemporaryFolder(parentDir);
+	@Rule
+	public TemporaryFolder testObjectDir = new TemporaryFolder(objParentDir);
 
 	@Mock
 	private static GameObject obj1;
@@ -40,6 +47,9 @@ public class MapSaveLoadTest {
 	public static void setUpBeforeClass() throws Exception {
 		if (!parentDir.exists()) {
 			parentDir.mkdirs();
+		}
+		if (!objParentDir.exists()) {
+			objParentDir.mkdirs();
 		}
 		
 		obj1 = mock(GameObject.class);
@@ -63,20 +73,121 @@ public class MapSaveLoadTest {
 	@Before
 	public void setUp() throws Exception {
 		mapSaveLoad = new MapSaveLoad();
+		
+		var redObj = testObjectDir.newFile("red.tobj");
+		try (var writer = new BufferedWriter(new FileWriter(redObj))) {
+			writer.write("50.0:60.0:FF0000");
+		}
+		
+		var failObj = testObjectDir.newFile("fail.tobj");
+		try (var writer = new BufferedWriter(new FileWriter(failObj))) {
+			writer.write("50.0:60.0");
+		}
+		
+		var correct = testMapsDir.newFile("correct.tmap");
+		try (var writer = new BufferedWriter(new FileWriter(correct))) {
+			writer.write( "100.0:70.0\n"
+						+ "15.0:10.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:50.0:30.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:40.0:30.0");
+		}
+		
+		var failOnMap = testMapsDir.newFile("failOnMap.tmap");
+		try (var writer = new BufferedWriter(new FileWriter(failOnMap))) {
+			writer.write( "100.0:70.0\n"
+						+ "15.0:10.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:50.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:20.0:10.0\n");
+		}
+		
+		var failOnObj = testMapsDir.newFile("failOnObj.tmap");
+		try (var writer = new BufferedWriter(new FileWriter(failOnObj))) {
+			writer.write( "100.0:70.0\n"
+						+ "15.0:10.0\n"
+						+ testObjectDir.getRoot().getName() + "/fail:50.0:30.0");
+		}
+		
+		var missingObject = testMapsDir.newFile("missingObject.tmap");
+		try (var writer = new BufferedWriter(new FileWriter(missingObject))) {
+			writer.write( "100.0:70.0\n"
+						+ "15.0:10.0\n"
+						+ testObjectDir.getRoot().getName() + "/test:50.0:30.0");
+		}
+		
+		var recover = testMapsDir.newFile("recover.tmap");
+		try (var writer = new BufferedWriter(new FileWriter(recover))) {
+			writer.write( "100.0:70.0\n"
+						+ "15.0:10.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:10.0:40.0\n"
+						+ testObjectDir.getRoot().getName() + "/test:50.0:20.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:20.0:40.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:70.0:40.0\n"
+						+ testObjectDir.getRoot().getName() + "/fail:50.0:30.0\n"
+						+ testObjectDir.getRoot().getName() + "/red:40.0:30.0");
+		}
 	}
 
 	@Test
-	public void testSave() throws IOException {
+	public void testSave() throws Exception {
 		var mapName = testMapsDir.getRoot().getName() + "/testSave";
 		mapSaveLoad.save(testMap, mapName);
 		var file = testMapsDir.getRoot().listFiles((f, n) -> n.startsWith("testSave."))[0];
 
 		try (var reader = new BufferedReader(new FileReader(file))) {
-			assertEquals("150.0/100.0", reader.readLine());
-			assertEquals("20.0/80.0", reader.readLine());
-			assertEquals("red/10.0/20.0", reader.readLine());
-			assertEquals("blue/100.0/50.0", reader.readLine());
+			assertEquals("150.0:100.0", reader.readLine());
+			assertEquals("20.0:80.0", reader.readLine());
+			assertEquals("red:10.0:20.0", reader.readLine());
+			assertEquals("blue:100.0:50.0", reader.readLine());
 		}
 	}
-
+	
+	@Test
+	public void testLoad() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/correct";
+		var map = mapSaveLoad.load(mapName, false);
+		
+		assertEquals(100.0, map.getWidth(), 0);
+		assertEquals(70.0, map.getHeight(), 0);
+		assertEquals(15.0, map.getPlayerX(), 0);
+		assertEquals(10.0, map.getPlayerY(), 0);
+		
+		assertEquals(2, map.getMapObjects().size());
+	}
+	
+	@Test(expected = AssetNotFoundException.class)
+	public void testLoadNotExisting() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/nonExisting";
+		mapSaveLoad.load(mapName, false);
+	}
+	
+	@Test(expected = AssetFormatException.class)
+	public void testLoadFalseFormat() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/failOnMap";
+		mapSaveLoad.load(mapName, false);
+	}
+	
+	@Test(expected = AssetFormatException.class)
+	public void testLoadFalseFormatInObject() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/failOnObj";
+		mapSaveLoad.load(mapName, false);
+	}
+	
+	@Test(expected = AssetNotFoundException.class)
+	public void testLoadMissingObject() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/missingObject";
+		mapSaveLoad.load(mapName, false);
+	}
+	
+	@Test
+	public void testLoadRecoverMode() throws Exception {
+		var mapName = testMapsDir.getRoot().getName() + "/recover";
+		var map = mapSaveLoad.load(mapName, true);
+		
+		assertEquals(100.0, map.getWidth(), 0);
+		assertEquals(70.0, map.getHeight(), 0);
+		assertEquals(15.0, map.getPlayerX(), 0);
+		assertEquals(10.0, map.getPlayerY(), 0);
+		
+		assertEquals(4, map.getMapObjects().size());
+	}
 }
