23d67dd6ba0c69309397748e3a625143b335c4a5
Armando Müller <muellarm@students.zhaw.ch>
2018-10-11 19:10:52 +0200

    Create ListItem class and add it to the objectExplorer

---
+++ src/main/java/ch/megil/teliaengine/ui/component/GameObjectListItem.java
@@ -0,0 +1,84 @@
+package ch.megil.teliaengine.ui.component;
+
+import java.util.function.Consumer;
+
+import ch.megil.teliaengine.game.GameObject;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+public class GameObjectListItem extends AnchorPane{
+	private Rectangle bg;
+	private HBox content;
+	
+	private ObjectProperty<GameObject> gameObject;
+	private ObjectProperty<Paint> hoverColor;
+	private ObjectProperty<Consumer<GameObject>> onAction;
+	
+	public GameObjectListItem() {
+		gameObject = new SimpleObjectProperty<>();
+		hoverColor = new SimpleObjectProperty<>();
+		onAction = new SimpleObjectProperty<>(obj -> {});
+		
+		bg = new Rectangle();
+		getChildren().add(bg);
+		content = new HBox();
+		content.setAlignment(Pos.CENTER_LEFT);
+		getChildren().add(content);
+		
+		bg.widthProperty().bind(widthProperty());
+		bg.heightProperty().bind(heightProperty());
+		
+		gameObject.addListener(this::gameObjectChangeListener);
+		
+		setOnMouseClicked(me -> onAction.get().accept(getGameObject()));
+		setOnMouseEntered(me -> swapColor());
+		setOnMouseExited(me -> swapColor());
+	}
+	
+	private void gameObjectChangeListener(ObservableValue<? extends GameObject> obj, GameObject oldObj, GameObject newObj) {
+		content.getChildren().clear();
+		content.getChildren().add(newObj.getDepiction());
+		content.getChildren().add(new Label(newObj.getName()));
+	}
+	
+	private void swapColor() {
+		if(hoverColor.get() != null) {
+			Paint old = bg.getFill();
+			bg.setFill(getHoverColor());
+			setHoverColor(old);
+		}
+	}
+	
+	
+	public GameObject getGameObject() {
+		return gameObject.get();
+	}
+	
+	public void setGameObject(GameObject obj) {
+		gameObject.set(obj);
+	}
+	
+	public Paint getHoverColor() {
+		return hoverColor.get();
+	}
+	
+	public void setHoverColor(Paint hoverColor) {
+		this.hoverColor.set(hoverColor);
+	}
+	
+	public Consumer<GameObject> getOnAction() {
+		return onAction.get();
+	}
+	
+	public void setOnAction(Consumer<GameObject> obj) {
+		onAction.set(obj);
+	}
+
+}
--- src/main/java/ch/megil/teliaengine/ui/component/ObjectExplorer.java
+++ src/main/java/ch/megil/teliaengine/ui/component/ObjectExplorer.java
@@ -2,47 +2,47 @@ package ch.megil.teliaengine.ui.component;
 
 import ch.megil.teliaengine.file.GameObjectSaveLoad;
 import ch.megil.teliaengine.game.GameObject;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-public class ObjectExplorer extends Pane{
+public class ObjectExplorer extends ScrollPane{
+	private VBox container;
+	
 	private MapEditor mapEditor;
-	private ListView<Node> list = new ListView<Node>();
 	
 	public ObjectExplorer() {
-		getChildren().addListener((ListChangeListener<Node>) c -> {
-			while(c.next()) {
-				c.getAddedSubList().forEach(n -> {
-					n.setOnMousePressed(this::createNewObject);
-					});
-			}});
+		setFitToWidth(true);
+		
+		container = new VBox();
+		container.setFillWidth(true);
+		setContent(container);
+		
+//		getChildren().addListener((ListChangeListener<Node>) c -> {
+//			while(c.next()) {
+//				c.getAddedSubList().forEach(n -> {
+//					n.setOnMousePressed(this::createNewObject);
+//					});
+//			}});
+		
 		new GameObjectSaveLoad().loadAll().forEach(this::loadGameObject);
-		this.getChildren().add(list);
+		
 	}
 	
 	public void loadGameObject(GameObject obj) {
-		getChildren().add(obj.getDepiction());
-		list.getItems().add(obj.getDepiction());
+		var listItem = new GameObjectListItem();
+		listItem.setGameObject(obj);
+		listItem.setHoverColor(Color.LIME);
+		listItem.setOnAction(this::createNewObject);
+		container.getChildren().add(listItem);
 	}
 	
-	public void createNewObject(MouseEvent event) {
-		if(event.isPrimaryButtonDown() && (!ListView.class.isInstance(event.getSource())) ) {
-			System.out.println(event.getSource());
-			var source = (Node) event.getSource();
-			getChildren().remove(source);
-			mapEditor.getChildren().add(source);
-			source.setLayoutX(mapEditor.getLayoutX());
-			source.setLayoutY(mapEditor.getLayoutY());
-		}
-	}
-	
-	public void fillList() {
-		
+	public void createNewObject(GameObject object) {
+		var newObject = new GameObjectSaveLoad().load(object.getName());
+		mapEditor.addGameObject(newObject);
 	}
 	
 	public void setMapEditor(MapEditor mapEditor) {
