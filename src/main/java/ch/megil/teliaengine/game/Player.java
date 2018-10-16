b20555666e09cff80b5e48aeff9c9cf051e4a4b7
Armando Müller <muellarm@students.zhaw.ch>
2018-10-16 22:09:01 +0200

    Refactor Vector and Player classes

--- src/main/java/ch/megil/teliaengine/game/Player.java
+++ src/main/java/ch/megil/teliaengine/game/Player.java
@@ -1,17 +1,23 @@
 package ch.megil.teliaengine.game;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Player {
+	private DoubleProperty posX;
+	private DoubleProperty posY;
 	private Vector position;
 	private Node depiction;
 	
 	public Player(Node depiction, double posX, double posY) {
 		this.depiction = new Rectangle(10, 10, Color.BLACK);
 		
-		this.position = new Vector(depiction.getLayoutX(), depiction.getLayoutY());
+		this.posX = new SimpleDoubleProperty(depiction.getLayoutX());
+		this.posY = new SimpleDoubleProperty(depiction.getLayoutY());
+		this.position = new Vector(posX, posY);
 		
 		this.depiction.layoutXProperty().bindBidirectional(position.getPosX());
 		this.depiction.layoutYProperty().bindBidirectional(position.getPosY());
--- src/main/java/ch/megil/teliaengine/game/Vector.java
+++ src/main/java/ch/megil/teliaengine/game/Vector.java
@@ -4,35 +4,43 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
 public class Vector {
-	private DoubleProperty posX;
-	private DoubleProperty posY;
+	private DoubleProperty x;
+	private DoubleProperty y;
 	
 	public Vector(double x, double y) {
-		this.posX = new SimpleDoubleProperty(x);
-		this.posY = new SimpleDoubleProperty(y);
-	}
-	
-	public void setX(double x) {
-		this.posX.set(x);
+		this.x = new SimpleDoubleProperty(x);
+		this.y = new SimpleDoubleProperty(y);
 	}
 	
 	public double getX() {
-		return posX.get();
+		return x.get();
 	}
 	
-	public void setY(double y) {
-		this.posY.set(y);
-	}
-	
-	public double getY() {
-		return posY.get();
+	public void setX(double x) {
+		this.y.set(x);
 	}
 	
 	public DoubleProperty getPosX() {
-		return posX;
+		return x;
+	}
+	
+	public void setDoublePropertyX(DoubleProperty x) {
+		this.x = x;
+	}
+	
+	public double getY() {
+		return y.get();
+	}
+	
+	public void setY(double y) {
+		this.y.set(y);
 	}
 	
 	public DoubleProperty getPosY() {
-		return posY;
+		return y;
+	}
+	
+	public void setDoublePropertyY(DoubleProperty y) {
+		this.y = y;
 	}
 }
