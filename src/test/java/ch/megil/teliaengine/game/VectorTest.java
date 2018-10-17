14f3d4181066529ce0ecff8fbd4560beb92ec08d
samichr1 <samichr1@students.zhaw.ch>
2018-10-17 11:42:18 +0200

    add vector test

---
+++ src/test/java/ch/megil/teliaengine/game/VectorTest.java
@@ -0,0 +1,19 @@
+package ch.megil.teliaengine.game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class VectorTest {
+	@Test
+	public void testVector() {
+		var vec = new Vector(5, 10);
+		
+		assertEquals(vec.getX(), 5, 0);
+		assertEquals(vec.getY(), 10, 0);
+		
+		assertEquals(vec.getPosX().get(), 5, 0);
+		assertEquals(vec.getPosY().get(), 10, 0);
+	}
+
+}
