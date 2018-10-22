d602087f39704b298cbe8a79c806696c4f24f0c6
samichr1 <samichr1@students.zhaw.ch>
2018-10-22 15:12:26 +0200

    add vector helpfunctions

--- src/main/java/ch/megil/teliaengine/game/Vector.java
+++ src/main/java/ch/megil/teliaengine/game/Vector.java
@@ -1,5 +1,9 @@
 package ch.megil.teliaengine.game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
@@ -44,5 +48,25 @@ public class Vector {
 	
 	public Vector add(Vector vector) {
 		return new Vector(this.getX() + vector.getX(), this.getY() + vector.getY());
+	}
+	
+	public Vector xVector() {
+		return new Vector(getX(), 0);
+	}
+	
+	public Vector yVector() {
+		return new Vector(0, getY());
+	}
+	
+	public List<Vector> splitToComponentSizeOne() {
+		var tx = (int) Math.abs(getX());
+		var ty = (int) Math.abs(getY());
+		if (tx == 0 && ty == 0) {
+			return new ArrayList<>();
+		} else if (tx > ty) {
+			return Collections.nCopies(tx, new Vector(getX()/tx, getY()/tx));
+		} else {
+			return Collections.nCopies(ty, new Vector(getX()/ty, getY()/ty));
+		}
 	}
 }
