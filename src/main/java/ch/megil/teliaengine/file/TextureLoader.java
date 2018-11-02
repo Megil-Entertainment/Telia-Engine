f3685c6f1df86a3cd0ca427e3c1915c56a7e3109
Armando Müller <muellarm@students.zhaw.ch>
2018-11-02 14:52:56 +0100

    Add TextureLoader

---
+++ src/main/java/ch/megil/teliaengine/file/TextureLoader.java
@@ -0,0 +1,25 @@
+package ch.megil.teliaengine.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import ch.megil.teliaengine.configuration.GameConfiguration;
+import ch.megil.teliaengine.file.exception.AssetFormatException;
+import ch.megil.teliaengine.file.exception.AssetNotFoundException;
+import javafx.scene.image.Image;
+
+public class TextureLoader {
+	public Image load(String name, double width, double height) throws AssetNotFoundException, AssetFormatException {
+		var fileName = GameConfiguration.ASSETS_TEXTURES.getConfiguration() + "/" + name + GameConfiguration.FILE_EXT_TEXTURE.getConfiguration();
+		var file = new File(fileName);
+		
+		try (var is = new FileInputStream(file)) {
+			var obj = new Image(is, width, height, false, false);
+			
+			return obj;
+		} catch (IOException e) {
+			throw new AssetNotFoundException("Texture not found: " + name, e);
+		}
+	}
+}
