bf9c2c8871915599108ff64ca940bda9752dd545
samichr1 <samichr1@students.zhaw.ch>
2018-11-06 14:51:49 +0100

    update vulkan exception for all res codes

--- src/main/java/ch/megil/teliaengine/vulkan/exception/VulkanException.java
+++ src/main/java/ch/megil/teliaengine/vulkan/exception/VulkanException.java
@@ -1,6 +1,11 @@
 package ch.megil.teliaengine.vulkan.exception;
 
-import static org.lwjgl.vulkan.VK10.VK_ERROR_INCOMPATIBLE_DRIVER;
+import static org.lwjgl.vulkan.EXTDebugReport.VK_ERROR_VALIDATION_FAILED_EXT;
+import static org.lwjgl.vulkan.KHRDisplaySwapchain.VK_ERROR_INCOMPATIBLE_DISPLAY_KHR;
+import static org.lwjgl.vulkan.KHRSurface.VK_ERROR_NATIVE_WINDOW_IN_USE_KHR;
+import static org.lwjgl.vulkan.KHRSurface.VK_ERROR_SURFACE_LOST_KHR;
+import static org.lwjgl.vulkan.KHRSwapchain.VK_ERROR_OUT_OF_DATE_KHR;
+import static org.lwjgl.vulkan.VK10.*;
 
 public class VulkanException extends Exception {
 	private static final long serialVersionUID = -8187611104093516686L;
@@ -8,17 +13,47 @@ public class VulkanException extends Exception {
 	public VulkanException(String message) {
 		super(message);
 	}
-	
+
 	public VulkanException(int resultCode) {
 		this(String.format("[Error Code: %d] %s", resultCode, translateVulkanResult(resultCode)));
 	}
-	
+
 	private static String translateVulkanResult(int result) {
-        switch (result) {
-	        case VK_ERROR_INCOMPATIBLE_DRIVER:
-	            return "Cannot find a compatible Vulkan Interface";
-	        default:
-	            return String.format("Unknown Error [%d]", result);
-        }
-    }
+		switch (result) {
+			case VK_ERROR_OUT_OF_HOST_MEMORY:
+				return "A host memory allocation has failed.";
+			case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+				return "A device memory allocation has failed.";
+			case VK_ERROR_INITIALIZATION_FAILED:
+				return "Initialization of an object could not be completed for implementation-specific reasons.";
+			case VK_ERROR_DEVICE_LOST:
+				return "The logical or physical device has been lost.";
+			case VK_ERROR_MEMORY_MAP_FAILED:
+				return "Mapping of a memory object has failed.";
+			case VK_ERROR_LAYER_NOT_PRESENT:
+				return "A requested layer is not present or could not be loaded.";
+			case VK_ERROR_EXTENSION_NOT_PRESENT:
+				return "A requested extension is not present, could not be loaded or is not supported.";
+			case VK_ERROR_FEATURE_NOT_PRESENT:
+				return "A requested feature is not supported.";
+			case VK_ERROR_INCOMPATIBLE_DRIVER:
+				return "The requested version of Vulkan is not supported by the driver or is otherwise incompatible for implementation-specific reasons.";
+			case VK_ERROR_TOO_MANY_OBJECTS:
+				return "Too many objects of the type have already been created.";
+			case VK_ERROR_FORMAT_NOT_SUPPORTED:
+				return "A requested format is not supported on this device.";
+			case VK_ERROR_SURFACE_LOST_KHR:
+				return "A surface is no longer available.";
+			case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
+				return "The requested window is already connected to a VkSurfaceKHR, or to some other non-Vulkan API.";
+			case VK_ERROR_OUT_OF_DATE_KHR:
+				return "A surface has changed in such a way that it is no longer compatible with the swapchain, and further presentation requests using the swapchain will fail. Applications must query the new surface properties and recreate their swapchain if they wish to continue presenting to the surface.";
+			case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
+				return "The display used by a swapchain does not use the same presentable image layout, or is incompatible in a way that prevents sharing an image.";
+			case VK_ERROR_VALIDATION_FAILED_EXT:
+				return "A validation layer found an error.";
+			default:
+				return String.format("Unknown Error [%d]", result);
+		}
+	}
 }
