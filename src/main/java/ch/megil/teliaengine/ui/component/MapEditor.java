8550bdbeee6f085ebc1cd9d4a95c699ad4aee0e3
Armando Müller <muellarm@students.zhaw.ch>
2018-11-16 13:40:30 +0100

    beginning of move camera in MapEditor

--- src/main/java/ch/megil/teliaengine/ui/component/MapEditor.java
+++ src/main/java/ch/megil/teliaengine/ui/component/MapEditor.java
@@ -5,14 +5,18 @@ import ch.megil.teliaengine.game.GameObject;
 import ch.megil.teliaengine.game.Map;
 import ch.megil.teliaengine.game.player.Player;
 import ch.megil.teliaengine.ui.GameElementImageView;
+import javafx.beans.binding.Bindings;
 import javafx.collections.ListChangeListener;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
-public class MapEditor extends Pane{
+public class MapEditor extends Pane {
 	private Map map;
 	private Player player;
+	private Pane pane;
 	
 	private double dx;
 	private double dy;
@@ -21,7 +25,16 @@ public class MapEditor extends Pane{
 	private int gridHeight;
 	
 	public MapEditor() {
-		getChildren().addListener((ListChangeListener<Node>) c -> {
+//		pane = new Pane();
+		pane = this;
+		var clip = new Rectangle();
+		clip.widthProperty().bind(this.widthProperty());
+		clip.heightProperty().bind(this.heightProperty());
+		pane.setClip(clip);
+		pane.setOnMousePressed(this::onMapDragStart);
+		pane.setOnMouseDragged(this::onDragMap);
+//		this.getChildren().add(pane);
+		pane.getChildren().addListener((ListChangeListener<Node>) c -> {
 			while(c.next()) {
 				c.getAddedSubList().forEach(n -> {
 					n.setOnMousePressed(this::onDragStart);
@@ -36,14 +49,25 @@ public class MapEditor extends Pane{
 	public void addGameObject(GameObject obj) {
 		if (map != null) {
 			map.addObject(obj);
-			getChildren().add(new GameElementImageView(obj));
+			pane.getChildren().add(new GameElementImageView(obj));
 		}
 	}
 	
 	public void onDragStart(MouseEvent event) {
-		var source = (Node) event.getSource();
-		dx = source.getLayoutX() - event.getSceneX();
-		dy = source.getLayoutY() - event.getSceneY();
+		if (event.isPrimaryButtonDown()) {
+			var source = (Node) event.getSource();
+			
+			dx = source.getLayoutX() - event.getSceneX();
+			dy = source.getLayoutY() - event.getSceneY();
+		}
+	}
+	
+	public void onMapDragStart(MouseEvent event) {
+		if (event.isSecondaryButtonDown()) {
+			var clip = (Rectangle)pane.getClip();
+			dx = clip.getX() - event.getSceneX();
+			dy = clip.getY() - event.getSceneY();
+		}
 	}
 	
 	private void moveNode(MouseEvent event) {
@@ -53,6 +77,14 @@ public class MapEditor extends Pane{
 		source.setImageViewLayoutY(
 				roundToNearest((event.getSceneY() + dy), gridHeight));
 		checkBoundries(source);
+	}
+	
+	private void moveMap(MouseEvent event) {
+		var clip = (Rectangle)pane.getClip();
+		clip.setX(event.getSceneX() + dx);
+		clip.setY(event.getSceneY() + dy);
+		pane.setTranslateX(0 - clip.getX());
+		pane.setTranslateY(0 - clip.getY());
 	}
 	
 	private double roundToNearest(double value, int roundFactor) {
@@ -86,6 +118,13 @@ public class MapEditor extends Pane{
 		}
 	}
 	
+	public void onDragMap(MouseEvent event) {
+		if(event.isSecondaryButtonDown()) {
+			moveMap(event);
+			event.consume();
+		}
+	}
+	
 	public Map getMap() {
 		return map;
 	}
@@ -98,9 +137,18 @@ public class MapEditor extends Pane{
 		getChildren().clear();
 		
 		this.map = map;
-		map.getMapObjects().forEach(o -> getChildren().add(new GameElementImageView(o)));
+		map.getMapObjects().forEach(o -> pane.getChildren().add(new GameElementImageView(o)));
 		this.player = Player.getEngineCopy();
-		getChildren().add(new GameElementImageView(player));
-		
+		pane.getChildren().add(new GameElementImageView(player));
+//		pane.setMinWidth(map.getWidth());
+//		pane.setMaxWidth(map.getWidth());
+//		pane.setMinHeight(map.getHeight());
+//		pane.setMaxHeight(map.getHeight());		
+//		pane.resize(map.getWidth(), map.getHeight());;
+		pane.setTranslateX(0);
+		pane.setTranslateY(0);
+		var clip = (Rectangle)pane.getClip();
+		clip.setX(0);
+		clip.setY(0);
 	}
 }
