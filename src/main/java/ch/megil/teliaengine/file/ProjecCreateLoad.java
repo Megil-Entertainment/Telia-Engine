3e0c7b7fa457f3b7465d865716d4f618ecd59137
samichr1 <samichr1@students.zhaw.ch>
2019-04-21 20:53:32 +0200

    add basic project structure creation

--- src/main/java/ch/megil/teliaengine/file/ProjecCreateLoad.java
+++ src/main/java/ch/megil/teliaengine/file/ProjecCreateLoad.java
@@ -6,7 +6,16 @@ import ch.megil.teliaengine.project.Project;
 
 public class ProjecCreateLoad {
 	public void initProject(Project project) {
+		var root = project.getLocationPath();
+		if (!new File(root).mkdirs()) {
+			throw new RuntimeException("directory error"); //TODO: new exception
+		}
 		
+		new File(root + "/assets/maps").mkdirs();
+		new File(root + "/assets/object").mkdirs();
+		new File(root + "/assets/texture").mkdirs();
+		new File(root + "/config").mkdirs();
+		new File(root + "/const").mkdirs();
 	}
 	
 	public Project loadProject(File projectLocation) {
