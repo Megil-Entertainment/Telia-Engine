27958a21f00a73160ab2f55023b04b22011ccc96
samichr1 <samichr1@students.zhaw.ch>
2019-05-02 10:32:22 +0200

    add basic creation dialog

--- src/main/java/ch/megil/teliaengine/Main.java
+++ src/main/java/ch/megil/teliaengine/Main.java
@@ -1,16 +1,21 @@
 package ch.megil.teliaengine;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 
 import ch.megil.teliaengine.configuration.FileConfiguration;
+import ch.megil.teliaengine.configuration.ProjectFolderConfiguration;
 import ch.megil.teliaengine.configuration.SystemConfiguration;
 import ch.megil.teliaengine.file.ProjectFileManager;
+import ch.megil.teliaengine.file.TextureFileManager;
 import ch.megil.teliaengine.file.exception.AssetCreationException;
 import ch.megil.teliaengine.file.exception.AssetLoadException;
 import ch.megil.teliaengine.logging.LogHandler;
 import ch.megil.teliaengine.project.ProjectController;
 import ch.megil.teliaengine.ui.FXMLConfiguration;
+import ch.megil.teliaengine.ui.dialog.ProjectCreateDialog;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,8 +24,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
@@ -54,7 +59,7 @@ public class Main extends Application {
 		
 		var retType = creationChooser.showAndWait().get();
 		if (retType == buttonNew) {
-			//TODO: create
+			openCreateDialog(projectFileManager);
 		} else if (retType == buttonOpen) {
 			openOpenDialog(stage, projectFileManager);
 		} else {
@@ -62,11 +67,44 @@ public class Main extends Application {
 		}
 	}
 	
+	private void openCreateDialog(ProjectFileManager projectFileManager) {
+		var project = new ProjectCreateDialog().showAndWait().get();
+		
+		if (project == null) {
+			System.exit(0);
+		} else {
+			try {
+				var projectInfo = projectFileManager.initProject(project);
+				projectFileManager.updateLastOpenedProject(projectInfo);
+				ProjectController.get().openProject(project);
+				//TODO: as soon as created: open ObjectCreator to create player and remove static player creation
+				TextureFileManager.get().importTexture("player", new File("assets/texture/player.png"));
+				var origin = new File("assets/player.tobj").toPath();
+				var dest = new File(ProjectFolderConfiguration.ASSET_PLAYER.getConfigurationWithProjectPath() + ".tobj").toPath();
+				try {
+					Files.copy(origin, dest);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				ProjectController.get().openProject(project);
+			} catch (AssetCreationException e) {
+				LogHandler.log(e, Level.SEVERE);
+				Alert error = new Alert(AlertType.ERROR);
+				error.setContentText("Could not create Project.");
+				error.showAndWait();
+				System.exit(-1);
+			}
+		}
+	}
+	
 	private void openOpenDialog(Stage stage, ProjectFileManager projectFileManager) {
 		var chooser = new FileChooser();
 		chooser.getExtensionFilters().add(new ExtensionFilter("Project", "*" + FileConfiguration.FILE_EXT_PROJECT.getConfiguration()));
 		var projectInfo = chooser.showOpenDialog(stage);
-		if (projectInfo != null) {
+		
+		if (projectInfo == null) {
+			System.exit(0);
+		} else {
 			try {
 				var project = projectFileManager.loadProject(projectInfo);
 				projectFileManager.updateLastOpenedProject(projectInfo);
@@ -80,8 +118,6 @@ public class Main extends Application {
 			} catch (AssetCreationException e) {
 				LogHandler.log(e, Level.WARNING);
 			}
-		} else {
-			System.exit(0);
 		}
 	}
 
