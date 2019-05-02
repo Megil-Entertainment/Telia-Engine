2068e50f3a4274391a8599997502132df7b2b1da
samichr1 <samichr1@students.zhaw.ch>
2019-05-02 15:23:35 +0200

    change projectcreatedialog to wizard

--- src/main/java/ch/megil/teliaengine/ui/dialog/ProjectCreateDialog.java
+++ src/main/java/ch/megil/teliaengine/ui/dialog/ProjectCreateDialog.java
@@ -3,33 +3,24 @@ package ch.megil.teliaengine.ui.dialog;
 import java.io.File;
 
 import ch.megil.teliaengine.project.Project;
+import ch.megil.teliaengine.ui.dialog.wizard.Wizard;
 import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 
-public class ProjectCreateDialog extends Dialog<Project> {
+public class ProjectCreateDialog extends Wizard<Project> {
 	private static final int PADDING = 15;
 	
-	private Node createBtn;
 	private TextField projectName;
 	private TextField location;
 	
 	public ProjectCreateDialog() {
-		var createType = new ButtonType("Create", ButtonData.OK_DONE);
-		getDialogPane().getButtonTypes().setAll(createType, ButtonType.CANCEL);
-		createBtn = getDialogPane().lookupButton(createType);
-		createBtn.setDisable(true);
-		
 		var grid = new GridPane();
 		grid.setPadding(new Insets(PADDING));
 		grid.setHgap(PADDING);
@@ -37,21 +28,21 @@ public class ProjectCreateDialog extends Dialog<Project> {
 		
 		grid.add(new Label("Project Name"), 0, 0);
 		projectName = new TextField();
-		projectName.textProperty().addListener(this::enableCreate);
+		projectName.textProperty().addListener(super::doNextPageCheckListener);
 		Platform.runLater(() -> projectName.requestFocus());
 		grid.add(projectName, 1, 0, 2, 1);
 		
 		grid.add(new Label("Location"), 0, 1);
 		location = new TextField();
-		location.textProperty().addListener(this::enableCreate);
+		location.textProperty().addListener(super::doNextPageCheckListener);
 		grid.add(location, 1, 1);
 		var searchBtn = new Button("...");
 		searchBtn.setOnAction(this::searchDirectory);
 		grid.add(searchBtn, 2, 1);
 		
-		getDialogPane().setContent(grid);
+		addPage(grid, this::checkFinish);
 		
-		setResultConverter(b -> b.equals(createType)
+		setResultConverter(b -> b.equals(ButtonType.FINISH)
 				? createProject(projectName.getText(), location.getText())
 				: null);
 	}
@@ -81,7 +72,7 @@ public class ProjectCreateDialog extends Dialog<Project> {
 		}
 	}
 	
-	private <T> void enableCreate(ObservableValue<? extends T> obs, T oldVal, T newVal) {
-		createBtn.setDisable(projectName.getText().trim().isEmpty() || location.getText().trim().isEmpty());
+	private boolean checkFinish() {
+		return projectName.getText().trim().isEmpty() || location.getText().trim().isEmpty();
 	}
 }
