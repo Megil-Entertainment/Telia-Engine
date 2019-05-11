fa193b31d9af0ee64898d44742935785bbadec77
Armando Müller <muellarm@students.zhaw.ch>
2019-05-11 13:59:41 +0200

    Create basic layout of ObjectCreateDialog

---
+++ src/main/java/ch/megil/teliaengine/ui/dialog/ObjectCreateDialog.java
@@ -0,0 +1,77 @@
+package ch.megil.teliaengine.ui.dialog;
+import ch.megil.teliaengine.game.GameObject;
+import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;;
+
+public class ObjectCreateDialog extends Dialog<GameObject>{
+	private static final int PADDING = 15;
+	
+	private Node createBtn;
+	private TextField objectName;
+	private TextField objectWidth;
+	private TextField objectHeight;
+	private TextField hitboxWidth;
+	private TextField hitboxHeight;
+	
+	public ObjectCreateDialog() {
+		var createType = new ButtonType("Create Object", ButtonData.OK_DONE);
+		getDialogPane().getButtonTypes().setAll(createType, ButtonType.CANCEL);
+		createBtn = getDialogPane().lookupButton(createType);
+		createBtn.setDisable(true);
+		
+		var grid = new GridPane();
+		grid.setPadding(new Insets(PADDING));
+		grid.setHgap(PADDING);
+		grid.setVgap(PADDING);
+		
+		grid.add(new Label("Name"), 0, 0);
+		objectName = new TextField();
+		objectName.textProperty().addListener(this::enableCreate);
+		Platform.runLater(() -> objectName.requestFocus());
+		grid.add(objectName, 1, 0);
+		
+		grid.add(new Label("Object Width"), 0, 1);
+		objectWidth = new TextField();
+		objectWidth.textProperty().addListener(this::enableCreate);
+		objectWidth.textProperty().addListener(this::checkInt);
+		grid.add(objectWidth, 1, 1);
+		
+		grid.add(new Label("Object Height"), 0, 2);
+		objectHeight = new TextField();
+		objectHeight.textProperty().addListener(this::enableCreate);
+		objectHeight.textProperty().addListener(this::checkInt);
+		grid.add(objectHeight, 1, 2);
+		
+		grid.add(new Label("Hitbox Width"), 0, 3);
+		hitboxWidth = new TextField();
+		hitboxWidth.textProperty().addListener(this::enableCreate);
+		hitboxWidth.textProperty().addListener(this::checkInt);
+		grid.add(hitboxWidth, 1, 3);
+		
+		grid.add(new Label("Hitbox Heigth"), 0, 4);
+		hitboxHeight = new TextField();
+		hitboxHeight.textProperty().addListener(this::enableCreate);
+		hitboxHeight.textProperty().addListener(this::checkInt);
+		grid.add(hitboxHeight, 1, 4);
+	}
+	
+	private void checkInt(ObservableValue<? extends String> obs, String oldVal, String newVal) {
+		if (!newVal.matches("\\d*")) {
+			((StringProperty)obs).set(newVal.replaceAll("[^\\d]", ""));
+		}
+	}
+	
+	private <T> void enableCreate(ObservableValue<? extends T> obs, T oldVal, T newVal) {
+		createBtn.setDisable(objectName.getText().trim().isEmpty() || objectWidth.getText().trim().isEmpty() || objectHeight.getText().trim().isEmpty()
+				|| hitboxWidth.getText().trim().isEmpty() || hitboxHeight.getText().trim().isEmpty());
+	}
+}
