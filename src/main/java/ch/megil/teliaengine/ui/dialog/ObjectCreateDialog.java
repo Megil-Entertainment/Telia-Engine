92b48915e3c7b18ea2c06d3bdbe06ba51b5bbb5c
Armando Müller <muellarm@students.zhaw.ch>
2019-05-27 21:54:47 +0200

    Remove unused imports

--- src/main/java/ch/megil/teliaengine/ui/dialog/ObjectCreateDialog.java
+++ src/main/java/ch/megil/teliaengine/ui/dialog/ObjectCreateDialog.java
@@ -34,7 +34,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.Image;
 
 public class ObjectCreateDialog extends Dialog<GameObject>{
 	private static final int PADDING = 15;
