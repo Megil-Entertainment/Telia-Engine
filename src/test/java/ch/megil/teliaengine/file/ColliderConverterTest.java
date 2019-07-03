e757810b0c1a832a0338340875d02c1a04615637
samichr1 <samichr1@students.zhaw.ch>
2019-07-03 15:20:50 +0200

    add string -> collider tests

--- src/test/java/ch/megil/teliaengine/file/ColliderConverterTest.java
+++ src/test/java/ch/megil/teliaengine/file/ColliderConverterTest.java
@@ -1,8 +1,9 @@
 package ch.megil.teliaengine.file;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,60 @@ public class ColliderConverterTest {
 	@Before
 	public void setUp() throws Exception {
 		colliderConverter = new ColliderConverter();
+	}
+
+	@Test
+	public void testConvertToColliderRectangle() {
+		var colliderStr = "rectangle:5.0:10.0:20.0:30.0";
+		
+		var collider = colliderConverter.convertToCollider(colliderStr);
+		assertThat(collider, instanceOf(RectangleCollider.class));
+		
+		var colliderTyped = (RectangleCollider)collider;
+		assertEquals(5.0, colliderTyped.getBoundingBoxBegin().getX(), 0.0);
+		assertEquals(10.0, colliderTyped.getBoundingBoxBegin().getY(), 0.0);
+		assertEquals(25.0, colliderTyped.getBoundingBoxEnd().getX(), 0.0);
+		assertEquals(40.0, colliderTyped.getBoundingBoxEnd().getY(), 0.0);
+	}
+	
+	@Test
+	public void testConvertToColliderCircle() {
+		var colliderStr = "circle:20.0:10.0:5.0";
+
+		var collider = colliderConverter.convertToCollider(colliderStr);
+		assertThat(collider, instanceOf(CircleCollider.class));
+
+		var colliderTyped = (CircleCollider)collider;
+		assertEquals(20.0, colliderTyped.getCenter().getX(), 0.0);
+		assertEquals(10.0, colliderTyped.getCenter().getY(), 0.0);
+		assertEquals(5.0, colliderTyped.getRadius(), 0.0);
+	}
+	
+	@Test
+	public void testConvertToColliderTriangle() {
+		var colliderStr = "triangle:5.0:5.0:15.0:4.0:3.0:18.0";
+
+		var collider = colliderConverter.convertToCollider(colliderStr);
+		assertThat(collider, instanceOf(TriangleCollider.class));
+
+		var colliderTyped = (TriangleCollider)collider;
+		assertEquals(5.0, colliderTyped.getP0().getX(), 0.0);
+		assertEquals(5.0, colliderTyped.getP0().getY(), 0.0);
+		assertEquals(15.0, colliderTyped.getP1().getX(), 0.0);
+		assertEquals(4.0, colliderTyped.getP1().getY(), 0.0);
+		assertEquals(3.0, colliderTyped.getP2().getX(), 0.0);
+		assertEquals(18.0, colliderTyped.getP2().getY(), 0.0);
+	}
+	
+	@Test
+	public void testConvertToColliderComposite() {
+		var colliderStr = "composite:;rectangle:5.0:10.0:20.0:30.0;circle:20.0:10.0:5.0;triangle:5.0:5.0:15.0:4.0:3.0:18.0";
+
+		var collider = colliderConverter.convertToCollider(colliderStr);
+		assertThat(collider, instanceOf(CompositeCollider.class));
+		
+		var colliderTyped = (CompositeCollider)collider;
+		assertEquals(3, colliderTyped.getColliders().size());
 	}
 	
 	@Test
